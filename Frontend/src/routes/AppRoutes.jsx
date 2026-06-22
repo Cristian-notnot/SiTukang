97c6f922222ec9a0d0ec878f032061a6265e13f4
nouval0425 <ghifarynopal@gmail.com>
2026-06-22 17:02:40 +0700

    solve : routes

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -1,55 +1,61 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import ProtectedRoute from "./ProtectedRoute";
 
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 
 import Home from "../pages/user/Home";
-
 import AdminDashboard from "../pages/admin/Dashboard";
-
 import TukangDashboard from "../pages/tukang/Dashboard";
 
 function AppRoutes() {
-
     return (
-
         <BrowserRouter>
-
             <Routes>
 
+                {/* Public Routes */}
                 <Route
-    path="/admin"
-    element={
-        <ProtectedRoute>
-            <AdminDashboard />
-        </ProtectedRoute>
-    }
-/>
+                    path="/login"
+                    element={<Login />}
+                />
 
-<Route
-    path="/tukang"
-    element={
-        <ProtectedRoute>
-            <TukangDashboard />
-        </ProtectedRoute>
-    }
-/>
+                <Route
+                    path="/register"
+                    element={<Register />}
+                />
 
-<Route
-    path="/user"
-    element={
-        <ProtectedRoute>
-            <Home />
-        </ProtectedRoute>
-    }
-/>
+                {/* Protected Routes */}
+                <Route
+                    path="/admin"
+                    element={
+                        <ProtectedRoute>
+                            <AdminDashboard />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/tukang"
+                    element={
+                        <ProtectedRoute>
+                            <TukangDashboard />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/user"
+                    element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    }
+                />
+
             </Routes>
-
         </BrowserRouter>
-
     );
-
 }
 
 export default AppRoutes;
